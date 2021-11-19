8c0ca99d54f6f1a73241cd0e745db9667f8a6b17
Riley <increaseblue666@gmail.com>
2021-11-19 21:31:20 +0900

    Feat : 엔티티 수정
    - area 엔티티 작성
    - user 엔티티에 마지막 사용시간 추가

--- src/area/area.entity.ts
+++ src/area/area.entity.ts
@@ -1,7 +1,16 @@
-import { Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Area {
   @PrimaryColumn()
-  id: string;
+  area_id: number;
+
+  @Column({ type: 'polygon' })
+  area_boundary: number;
+
+  @Column({ type: 'point' })
+  area_center: number;
+
+  @Column({ type: 'linestring' })
+  area_coords: number;
 }
