d4cbfb9ef088f44318693feb5dec0063f7a80aa4
chunu-j <codehouse9@naver.com>
2021-11-20 01:43:37 +0900

    Modify: validationAndReturnArea null 반환에서 err메시지 수정

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Area } from './entities/area.entity.ts';
+import { Area } from './area.entity';
 
 @Injectable()
 export class AreaService {
@@ -12,17 +12,20 @@ export class AreaService {
 
   async validationAndReturnArea(areaId: string): Promise<Area> {
     //todo 지역의 유효성 검증 (유효하지 않을 경우 throw error)
-    //todo 지역 entity를 반환
+    //todo 지역 entity 를 반환
     const areaEntity = await this.findOne(areaId);
     if (areaEntity.length) {
       return areaEntity;
     } else {
-      return null; // 없을 경우 어떻게 처리할지 몰라서 일단 null
+      throw new NotFoundException(`"${areaId}"가 존재하지 않습니다.`);
     }
   }
 
   //todo 사용자 이용의 위도, 경도가 지역의 폴리곤 범위를 벗어나는지 확인하는 메서드
-  async checkOutOfRange(useEndLat: number, useEndLng: number): Promise<boolean> {
+  async checkOutOfRange(
+    useEndLat: number,
+    useEndLng: number,
+  ): Promise<boolean> {
     //todo 범위 확인 로직
     const qPoint = `'POINT (${useEndLat} ${useEndLng})'`;
     const query = ` SELECT area_id FROM area 
