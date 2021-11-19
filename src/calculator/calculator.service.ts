ca5611ebe99743f2a0f4cfb0fd9271078c2c5945
chunu-j <codehouse9@naver.com>
2021-11-20 01:27:19 +0900

    Feat: 포비든서비스에 '거리반환', '금지구역확인' 추가

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -13,18 +13,18 @@ export class CalculatorService {
     private readonly regularPoliciesService: RegularPoliciesService,
     private readonly forbiddenAreaService: ForbiddenAreaService,
   ) {}
-  calculateRate(use: UseDto): any {
+  async calculateRate(use: UseDto): Promise<any> {
     //todo Kickboard의 유효성 및 Area ID 반환
     const areaId = this.kickboardsService.validationAndReturnAreaInfo(
       use.useDeerName,
     );
     //todo 지역의 유효성 및 지역 Entity 반환
-    const area = this.areaService.validationAndReturnArea(areaId);
+    const area = this.areaService.validationAndReturnArea(await areaId);
     //todo 이용시간 계산
     const usedTime = this.calculateUsedTime(use.useStartAt, use.useEndAt);
     //todo 지역 아이디를 기반으로 해당 지역의 기본요금을 계산
     const regularRate = this.regularPoliciesService.calculateRegularRate(
-      areaId,
+      await areaId,
       usedTime,
     );
     //todo 지역 폴리곤 벗어났는지 확인. area.service 로 이동. 안 = true 바깥 = false 반환
@@ -33,11 +33,11 @@ export class CalculatorService {
       use.useEndLng,
     );
     // outOfRange == false 일때 바깥으로 부터 몇 m 떨어져있는지 확인하는 메소드.
-    // -> ForbiddenService 의 outsideDistance(area, use.useEndLat, use.useEndLng);
-    // todo 금지구역에 있는지 확인 outOfRange == true
+    // const 몇미터? = this.forbiddenAreaService.outsideDistance(area, use.useEndLat, use.useEndLng);
+
+    // todo 금지구역에 있는지 확인. outOfRange == true
     const checkInsideForbiddenArea =
       this.forbiddenAreaService.checkInsideForbiddenArea(
-        areaId,
         use.useEndLat,
         use.useEndLng,
       );
